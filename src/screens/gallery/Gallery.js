661d1e54cd17485b85056d73c8c41dd6178ac4da
VaibhavRajai <rajaivaibhav28@gmail.com>
2023-06-30 13:07:39 +0530

    Updated the Photo Gallery and added global stylesheet

--- src/screens/gallery/Gallery.js
+++ src/screens/gallery/Gallery.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View, StyleSheet, FlatList, Image, ScrollView, TouchableOpacity, Linking } from 'react-native';
-// import styles from './Gallery.styles';
+import styles from './Gallery.styles';
 
 import firestore from '@react-native-firebase/firestore';
 
@@ -43,64 +43,4 @@ const Gallery = () => {
 	);
 };
 
-const styles = StyleSheet.create({
-	card: {
-		backgroundColor: '#912929',
-		borderRadius: 8,
-		padding: 16,
-		shadowColor: '#000',
-		shadowOpacity: 0.2,
-		shadowRadius: 4,
-		elevation: 2,
-		height: 180,
-		width: 180,
-		marginHorizontal: 8,
-		margin: 10
-	},
-	main: {
-		flex: 1,
-		alignItems: 'center',
-		backgroundColor: '#fef7d7'
-	},
-	title: {
-		fontSize: 25,
-		fontWeight: 'bold',
-		marginBottom: 8,
-		color: 'white',
-		fontFamily: 'arial'
-	},
-	description: {
-		fontSize: 20,
-		color: '#c1ccde',
-		fontFamily: 'Poppins'
-	},
-	body: {
-		backgroundColor: '#fef7d7',
-		flex: 1
-	},
-	photoItem: {
-		width: 20,
-		height: 100,
-		margin: 5
-	},
-	text: {
-		backgroundColor: '#912929',
-		padding: 10,
-		borderRadius: 5,
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		fontFamily: 'arial',
-		fontWeight: 'bold',
-		marginLeft: 15,
-		marginRight: 67
-	},
-
-	image: {
-		width: 300,
-		height: 250,
-		resizeMode: 'cover',
-		margin: 20
-	}
-});
 export default Gallery;
