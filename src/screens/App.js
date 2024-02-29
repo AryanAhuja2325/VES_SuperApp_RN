405212dff7b95bbba6e3a01057517bdf4934c297
KavadeSakshi <co2021.sakshi.kavade@ves.ac.in>
2024-02-29 09:35:22 +0530

    .......

--- src/screens/App.js
+++ src/screens/App.js
@@ -22,10 +22,7 @@ import Details from './stationarySupply/Details';
 import Cart from './stationarySupply/Cart';
 import Fees from './fees/Fees';
 import Splash from './splash';
-import AddJob from './placement/AddJob';
-import JobDetails from './placement/JobDetails';
 import Chat from './groupchat/chat';
-
 import FAQ from './FAQs/faqs';
 import Profile from './bottomTab/profile';
 import Notifications from './bottomTab/notifications';
@@ -36,7 +33,6 @@ import CustomHeader from '../components/header';
 import FitnessAndHealth from './fitnessandhealth/FitnessAndHealth';
 import DigitalAcademy from './digitalAcademy/DigitalAcademy';
 import DigitalAcademyDetail from './digitalAcademy/DigitalAcademyDetail';
-import Placement from './placement/Placement';
 import ImageGrid from './photoGallery';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { white } from '../utils/color';
@@ -385,37 +381,8 @@ const App = () => {
               ),
             })}
           />
-          <stack.Screen
-            name="Placement"
-            component={Placement}
-            options={({ navigation }) => ({
-              headerShown: true,
-              header: () => (
-                <CustomHeader navigation={navigation} title="Placement" />
-              ),
-            })}
-          />
-          <stack.Screen
-            name="JobDetails"
-            component={JobDetails}
-            options={({ navigation }) => ({
-              headerShown: true,
-              header: () => (
-                <CustomHeader navigation={navigation} title="JobDetails" />
-              ),
-            })}
-          />
-          <stack.Screen
-            name="AddJob"
-            component={AddJob}
-            options={({ navigation }) => ({
-              headerShown: true,
-              header: () => (
-                <CustomHeader navigation={navigation} title="AddJob" />
-              ),
-            })}
-          />
-
+        
+          
           <stack.Screen
             name="Blog"
             component={Blog}
